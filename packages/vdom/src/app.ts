104aabe9ac41a526b27cfda6b391fc91e59380d4
ssssota <tommysouta@gmail.com>
2024-06-01 20:06:46 +0900

    refactor: remove render function

--- packages/vdom/src/app.ts
+++ packages/vdom/src/app.ts
@@ -1,41 +1,81 @@
 import type { Component } from "./component";
 import type { VNode } from "./vnode";
 
-function render(node: string | VNode): Node {
-	if (typeof node === "string") {
-		return document.createTextNode(node);
+function patchAttrs(
+	el: HTMLElement,
+	oldAttrs: Record<string, unknown>,
+	newAttrs: Record<string, unknown>,
+) {
+	const oldKeys = Object.keys(oldAttrs);
+	const oldEventHandlers = oldKeys.filter((key) => key.startsWith("on"));
+	for (const key of oldEventHandlers) {
+		const eventName = key.slice(2).toLowerCase();
+		// @ts-ignore
+		el.removeEventListener(eventName, oldAttrs[key]);
 	}
-
-	const el = document.createElement(node.tag);
-	for (const [key, value] of Object.entries(node.props)) {
+	for (const [key, value] of Object.entries(newAttrs)) {
 		if (key.startsWith("on")) {
+			const eventName = key.slice(2).toLowerCase();
 			// @ts-ignore
-			el.addEventListener(key.slice(2).toLowerCase(), value);
-		} else {
+			el.addEventListener(eventName, value);
+		} else if (oldAttrs[key] !== value) {
 			el.setAttribute(key, String(value));
 		}
 	}
-	for (const child of node.children) {
-		el.appendChild(render(child));
+	for (const key of oldKeys) {
+		if (!(key in newAttrs)) {
+			el.removeAttribute(key);
+		}
 	}
-	return el;
+}
+
+function patchChildren(
+	parent: HTMLElement,
+	oldChildren: (string | VNode)[],
+	newChildren: (string | VNode)[],
+) {
+	const length = Math.max(oldChildren.length, newChildren.length);
+	for (let i = 0; i < length; i++) {
+		patch(parent, oldChildren[i], newChildren[i], i);
+	}
 }
 
 function patch(
 	parent: Node,
 	oldNode: VNode | string | undefined,
-	newNode: VNode | string,
+	newNode: VNode | string | undefined,
 	index = 0,
 ) {
+	if (newNode === undefined) {
+		if (oldNode === undefined) return;
+		parent.removeChild(parent.childNodes[index]);
+		return;
+	}
 	if (oldNode === undefined) {
-		parent.appendChild(render(newNode));
+		if (typeof newNode === "string") {
+			parent.appendChild(document.createTextNode(newNode));
+			return;
+		}
+		const newEl = document.createElement(newNode.tag);
+		patchAttrs(newEl, {}, newNode.props);
+		patchChildren(newEl, [], newNode.children);
+		parent.appendChild(newEl);
 		return;
 	}
 	const el = parent.childNodes[index];
-	if (typeof oldNode === "string" || typeof newNode === "string") {
-		if (oldNode !== newNode) {
-			parent.replaceChild(render(newNode), el);
+	if (typeof oldNode === "string") {
+		if (typeof newNode === "string") {
+			if (oldNode !== newNode) {
+				el.replaceWith(document.createTextNode(newNode));
+			}
+			return;
 		}
+		parent.removeChild(el);
+		patch(parent, undefined, newNode, index);
+		return;
+	}
+	if (typeof newNode === "string") {
+		el.replaceWith(document.createTextNode(newNode));
 		return;
 	}
 	if (!(el instanceof HTMLElement)) throw new Error("unexpected node");
@@ -44,26 +84,13 @@ function patch(
 		oldNode.tag.toLowerCase() !== newTag ||
 		el.tagName.toLowerCase() !== newTag
 	) {
-		el.replaceWith(render(newNode));
+		const newEl = document.createElement(newTag);
+		patchAttrs(newEl, {}, newNode.props);
+		patchChildren(newEl, [], newNode.children);
 		return;
 	}
-	for (const [key, value] of Object.entries(newNode.props)) {
-		if (key.startsWith("on")) {
-			const eventName = key.slice(2).toLowerCase();
-			// @ts-ignore
-			el.removeEventListener(eventName, oldNode.props[key]);
-			// @ts-ignore
-			el.addEventListener(eventName, value);
-		} else if (oldNode.props[key] !== value) {
-			el.setAttribute(key, String(value));
-		}
-	}
-	const oldChildren = oldNode.children;
-	const newChildren = newNode.children;
-	const length = Math.min(oldChildren.length, newChildren.length);
-	for (let i = 0; i < length; i++) {
-		patch(el, oldChildren[i], newChildren[i], i);
-	}
+	patchAttrs(el, oldNode.props, newNode.props);
+	patchChildren(el, oldNode.children, newNode.children);
 }
 
 export function createApp<Props extends Record<string, unknown>>(
