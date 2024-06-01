c5646789899d389925ac81a23daf1dcfedfe9525
ssssota <tommysouta@gmail.com>
2024-06-01 19:33:56 +0900

    feat: add buggy diffing algorithm

--- packages/vdom/src/app.ts
+++ packages/vdom/src/app.ts
@@ -21,23 +21,66 @@ function render(node: string | VNode): Node {
 	return el;
 }
 
+function patch(
+	parent: Node,
+	oldNode: VNode | string | undefined,
+	newNode: VNode | string,
+	index = 0,
+) {
+	if (oldNode === undefined) {
+		parent.appendChild(render(newNode));
+		return;
+	}
+	const el = parent.childNodes[index];
+	if (typeof oldNode === "string" || typeof newNode === "string") {
+		if (oldNode !== newNode) {
+			parent.replaceChild(render(newNode), el);
+		}
+		return;
+	}
+	if (!(el instanceof HTMLElement)) throw new Error("unexpected node");
+	const newTag = newNode.tag.toLowerCase();
+	if (
+		oldNode.tag.toLowerCase() !== newTag ||
+		el.tagName.toLowerCase() !== newTag
+	) {
+		el.replaceWith(render(newNode));
+		return;
+	}
+	for (const [key, value] of Object.entries(newNode.props)) {
+		if (key.startsWith("on")) {
+			const eventName = key.slice(2).toLowerCase();
+			// @ts-ignore
+			el.removeEventListener(eventName, oldNode.props[key]);
+			// @ts-ignore
+			el.addEventListener(eventName, value);
+		} else if (oldNode.props[key] !== value) {
+			el.setAttribute(key, String(value));
+		}
+	}
+	const oldChildren = oldNode.children;
+	const newChildren = newNode.children;
+	const length = Math.min(oldChildren.length, newChildren.length);
+	for (let i = 0; i < length; i++) {
+		patch(el, oldChildren[i], newChildren[i], i);
+	}
+}
+
 export function createApp<Props extends Record<string, unknown>>(
 	root: Component<Props>,
 ) {
 	let _target: HTMLElement;
-	function _render(props: Props) {
+	let _prev: VNode;
+	function rerender(props: Props) {
 		const vnode = root(props);
-		const el = render(vnode);
-		_target.appendChild(el);
+		patch(_target, _prev, vnode);
+		_prev = vnode;
 	}
 	return {
 		mount(target: HTMLElement, props: Props) {
 			_target = target;
-			_render(props);
+			rerender(props);
 		},
-		rerender(props: Props) {
-			_target.textContent = "";
-			_render(props);
-		},
+		rerender,
 	};
 }
